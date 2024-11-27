d6917dd1eb5be9247f479d0ea5a8e274761a5137
koya <koya9431ptr@gmail.com>
2024-11-27 19:29:00 +0000

    done

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-$(function() {
+$(function () {
   // your code here
+  $(".accordion-header").click(function () {
+    $(".accordion-content").slideUp();
+
+    if (!$(this).next().is(":visible")) {
+      $(this).next().slideDown();
+    }
+  });
+
+  $("button").click(function () {
+    $.ajax({
+      url: "https://dummyjson.com/todos",
+      method: "GET",
+      success: function (data) {
+        const todos = data.todos;
+        const ul = $(".todos ul");
+        ul.empty();
+        todos.forEach(function (todo) {
+          ul.append(`<li>${todo.todo}</li>`);
+        });
+      },
+      error: function (error) {
+        console.error("Error items:", error);
+      }
+    });
+  });
 })
